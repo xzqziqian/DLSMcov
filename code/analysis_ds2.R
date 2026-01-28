0a8f86024a131366c1267b30a87173ff64915aa3
Ziqian Xu <38840988+xzqziqian@users.noreply.github.com>
2026-01-28 09:10:47 -0500

    update info

--- code/analysis_ds2.R
+++ code/analysis_ds2.R
@@ -1,3 +1,8 @@
+# 
+# Extension of Sewell & Chen (2015) to include dynamic non-network covariate evolution. Based on the dynamic latent space model code from https://publish.illinois.edu/yuguo/software/ and https://sewell.lab.uiowa.edu/software
+# 
+# Reference: Sewell, D. K., & Chen, Y. (2015). Latent space models for dynamic networks. Journal of the american statistical association, 110(512), 1646-1657.
+
 setwd("~/Library/CloudStorage/GoogleDrive-zxu9@nd.edu/My Drive/projects/DLSMcov/shandong")
 load("shandong.RData")
 Y.pre = shandong[[1]][1:3]
--- code/initialize_ds2.R
+++ code/initialize_ds2.R
@@ -1,3 +1,10 @@
+
+# 
+# Extension of Sewell & Chen (2015) to include dynamic non-network covariate evolution. Based on the dynamic latent space model code from https://publish.illinois.edu/yuguo/software/ and https://sewell.lab.uiowa.edu/software
+# 
+# Reference: Sewell, D. K., & Chen, Y. (2015). Latent space models for dynamic networks. Journal of the american statistical association, 110(512), 1646-1657.
+
+
 #######################
 ###     Startup     ###
 #######################
